2c0ecf8674ceebcff507de17ad1c64ec3ac53465
Jakh0n <jakhonyokubjonov@gmail.com>
2025-01-22 11:42:26 +0900

    User. API

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,8 +2,15 @@ const router = require('express').Router()
 const adminController = require('../controllers/admin.controller')
 
 router.get('/products', adminController.getProduct)
+router.get('/customers', adminController.getCustomers)
+router.get('/orders', adminController.getOrders)
+router.get('/transactions', adminController.getTransactions)
+
 router.post('/create-product', adminController.createProduct)
+
 router.put('/update-product/:id', adminController.updateProduct)
+router.put('/update-order/:id', adminController.updateOrder)
+
 router.delete('/delete-product/:id', adminController.deleteProduct)
 
 module.exports = router
